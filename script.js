8e519ef5ac17c1b12506be7f752c15de2ff35315
ahmedali8 <imahmedalibhatti@gmail.com>
2020-08-08 19:44:44 +0500

    updated script.js to display balance, income and expense

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
 const dummyTransactions = [
-    {id: 1, text: 'Flower', amount: -20},
+    {id: 1, text: 'Cat', amount: -20},
     {id: 2, text: 'Salary', amount: 300},
     {id: 3, text: 'Book', amount: -10},
     {id: 4, text: 'Camera', amount: 150}
